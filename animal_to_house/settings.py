ce0c790b26b29ccc43f0601e0135decc6d55afc3
im83 <programmer38rus@gmail.com>
2020-10-31 20:59:18 +0800

    New

--- animal_to_house/settings.py
+++ animal_to_house/settings.py
@@ -22,7 +22,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'c*%hdd5r0!ch3cn38s1&1-7s3u=u84+*(hv3s6&5z(7r^&oz21'
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
